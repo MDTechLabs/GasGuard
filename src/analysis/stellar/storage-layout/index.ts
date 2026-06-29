2f026fad7b1645e6fe56e25a21dbec5b7768fa45
northersubair <northersubair@users.noreply.github.com>
2026-06-29 15:23:46 +0100

    Add SLOAD, memory copy, signature recovery rules and storage layout analyzer

---
+++ src/analysis/stellar/storage-layout/index.ts
@@ -0,0 +1,6 @@
+export { SorobanStorageLayoutAnalyzer } from "./storage-layout-analyzer";
+export type {
+  StorageLayoutEntry,
+  StorageLayoutWarning,
+  StorageLayoutReport,
+} from "./storage-layout-analyzer";
